b14f3876dd08204117342340a8115ac57f1b84f0
Lane Faison <lane.faison@icloud.com>
2017-06-30 17:57:58 -0600

    Initial commit

---
+++ Scribe/CircleButton.swift
@@ -0,0 +1,27 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Lane Faison on 6/30/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 30.0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    func setupView() {
+        layer.cornerRadius = cornerRadius
+    }
+}
