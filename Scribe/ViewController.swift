f1283c1060bcf2dab0ccccc2bfdaf7c840042f8b
Lane Faison <lane.faison@icloud.com>
2017-06-30 18:20:12 -0600

    Updates

--- Scribe/ViewController.swift
+++ Scribe/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var transcriptionTextField: UITextView!
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        activitySpinner.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        sound.play()
+                    } catch {
+                        print("Error!")
+                    }
+                    
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result,error) in
+                        if let error = error {
+                            print("There was an error: \(error)")
+                        } else {
+                            print(result?.bestTranscription.formattedString)
+                        }
+                }
+            }
+        }
     }
-
 
 }
 
